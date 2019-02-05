3176fc186c76b655f6d4834657e587bfa3a715f3
dtorrenp <40465310+dtorrenp@users.noreply.github.com>
2019-02-05 14:07:04 +0000

    Update Open_fits.py
    python now does the same as topcat in creating sub-tables

--- Open_fits.py
+++ Open_fits.py
@@ -5,22 +5,17 @@ from astropy.table import Table
 """Open fits file and create "Table" object with which actual data can be accessed"""
 
 hdulist = fits.open('/Users/daniel/Documents/Bsc_Project/COM_PCCS-Catalogs_vPR2/COM_PCCS_857-excluded_R2.01.fits')
-                 
-print("FITS FILE INFO")
-hdulist.info()
 hdu_Primary = hdulist[0]
 hdu_BinTable = hdulist[1]
-#print("PRIMARY SHAPE")
-#print(hdu_Primary.data)
-#print("PRIMARY HEADER")
-#print(hdu_Primary.header)
-#print("BinTable SHAPE")
-#print(hdu_BinTable.data.shape)
-#print("BinTable HEADER")
-#print(hdu_BinTable.header)
 
-print("Table Data")
 t_data = Table(hdu_BinTable.data)
-print(t_data)
+colnames = t_data.colnames
 #%%
+def subset_table(table,column,condition):
+    subset = table[table[column] == condition]
+    print(subset)
+    return subset
+#%%
+extended_sub_table = subset_table(t_data,"EXTENDED",0)
+extended_external_sub_sub_table = subset_table(extended_sub_table,"EXT_VAL",3)
 
