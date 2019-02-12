0e6738ba9eb1db7621aa808a54f4b8f7e3178cda
Unknown <dt1716@ic.ac.uk>
2019-02-12 12:12:54 +0000

    open the sdss fits file

--- Open_fits.py
+++ Open_fits.py
@@ -1,21 +1,42 @@
 #%%
 from astropy.io import fits
 from astropy.table import Table
+import timeit
 #%%
 """Open fits file and create "Table" object with which actual data can be accessed"""
-
-hdulist = fits.open('/Users/daniel/Documents/Bsc_Project/COM_PCCS-Catalogs_vPR2/COM_PCCS_857-excluded_R2.01.fits')
-hdu_Primary = hdulist[0]
-hdu_BinTable = hdulist[1]
-
-t_data = Table(hdu_BinTable.data)
-colnames = t_data.colnames
+start = timeit.default_timer()
+#Planck data
+#hdulist_PLANCK = fits.open('/Users/daniel/Documents/Bsc_Project/COM_PCCS-Catalogs_vPR2/COM_PCCS_857-excluded_R2.01.fits')
+#hdu_Primary_PLANCK = hdulist_PLANCK[0]
+#hdu_BinTable_PLANCK = hdulist_PLANCK[1]
+#t_primary_PLANCK  = Table(hdu_Primary_PLANCK.data)
+#t_data_PLANCK = Table(hdu_BinTable_PLANCK.data)
+#colnames_PLANCK = t_data_PLANCK.colnames
+print("hi")
+#SDSS data
+hdulist_SDSS = fits.open('/Users/daniel/Documents/Bsc_Project/\SDSS data dr14/specObj-dr14.fits')
+print("hi")
+hdu_Primary_SDSS = hdulist_SDSS[0]
+print("hi")
+hdu_BinTable_SDSS = hdulist_SDSS[1]
+print("hi")
+t_primary_SDSS  = Table(hdu_Primary_SDSS.data)
+print("hi")
+t_data_SDSS = Table(hdu_BinTable_SDSS.data)
+print("hi")
+colnames_SDSS = t_data_SDSS.colnames
+print("hi")
+end = timeit.default_timer()
+print(end-start)
 #%%
 def subset_table(table,column,condition):
-    subset = table[table[column] == condition]
-    print(subset)
+    mask = table[column] == condition
+    print(len(mask))
+    subset = table[mask]
+    #print(subset)
     return subset
 #%%
-extended_sub_table = subset_table(t_data,"EXTENDED",0)
-extended_external_sub_sub_table = subset_table(extended_sub_table,"EXT_VAL",3)
-
+#extended_sub_table = subset_table(t_data,"EXTENDED",0)#subset with only extened flag equal to 0
+#one_extended_sub_table = subset_table(t_data,"EXTENDED",1)
+#
+#extended_external_sub_sub_table = subset_table(extended_sub_table,"EXT_VAL",3)#subset within reduced table with ext_val equal to 3
