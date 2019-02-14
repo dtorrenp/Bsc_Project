667329b58766aca20d451971999f9baef5afe512
Unknown <dt1716@ic.ac.uk>
2019-02-14 12:49:25 +0000

    select object with largest flux

--- Open_fits.py
+++ Open_fits.py
@@ -6,7 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 #%%
 """Open fits file and create "Table" object with which actual data can be accessed"""
-#start = timeit.default_timer()
+start = timeit.default_timer()
 ##Planck data
 #hdulist_PLANCK = fits.open('/Users/daniel/Documents/Bsc_Project/COM_PCCS-Catalogs_vPR2/COM_PCCS_857-excluded_R2.01.fits')
 ##hdu_Primary_PLANCK = hdulist_PLANCK[0]
@@ -30,71 +30,84 @@ hdulist_SDSS = fits.open('/Users/daniel/Documents/Bsc_Project/SDSS data dr14/sub
 hdu_BinTable_SDSS = hdulist_SDSS[1]
 ##t_primary_SDSS  = Table(hdu_Primary_SDSS.data)
 t_data_SDSS = Table(hdu_BinTable_SDSS.data)
+
 colnames_SDSS = t_data_SDSS.colnames
 
-#end = timeit.default_timer()
-#print(end-start)
 
 
+reduced_table = t_data_SDSS['CLASS','Z','SPECTROFLUX','GroupID','GroupSize']
+#create new table with the ojects within each compact source that has the largest flux
+# create table of those with no tag
+mask_reduced_no_tag = reduced_table['GroupID'] < 1
+reduced_no_tag = reduced_table[mask_reduced_no_tag]#CORRECT
+#print(len(reduced_no_tag))
+
+mask_reduced_groups = reduced_table['GroupID'] > 0
+reduced_groups = reduced_table[mask_reduced_groups]#CORRECT
+#print(len(reduced_groups))
+
+
+#ugriz  filters
+f = 1
+
+for i in np.arange(1,max(reduced_groups['GroupID'])+1):
+    group_mask  = reduced_groups['GroupID'] == i
+    red_group_i = reduced_groups[group_mask]
+    
+    flux_list_filter = []
+    for v in np.arange(red_group_i['GroupSize'][0]):
+        fluxes = np.asarray(red_group_i['SPECTROFLUX'][v])
+        flux_filter = fluxes[f]
+        flux_list_filter.append(flux_filter)
+    largest_flux = max(flux_list_filter)
+    index_flux = flux_list_filter.index(largest_flux)
+    largest_flux_object = red_group_i[index_flux]
+    reduced_no_tag.add_row(largest_flux_object)
+
+galactic_stars = reduced_no_tag[reduced_no_tag['CLASS'] == "STAR"]
+planck_galaxies = reduced_no_tag[reduced_no_tag['CLASS'] == "GALAXY"]
 
 #%%
-#def select_largest_mag(table,f):
-#    
-#    
-#    
-#    
-#    array_table_flux = np.array(table["SPECTROFLUX"])
-#    
-#    
-#    
-#    return sub_table
 
 def cal_abs_mag(table,f):
     #ugriz  filters
     array_table_flux = np.array(table["SPECTROFLUX"])
     
     array_flux_f = np.abs(array_table_flux[:,f])
-#    print("array_flux_f")
-#    print(array_flux_f)
     #print(array_flux_f[array_flux_f < 0])#WEIRD SHIT WITH NEGATIVE FLUXES
     
     array_flux_f_plus = np.abs(array_table_flux[:,f-1])
-#    print("array_flux_f_plus")
-#    print(array_flux_f_plus)
     
     z_val = np.array(table["Z"])
-#    print("z_val")
-#    print(z_val)
     
     filter_m_f = 22.5 - 2.5*np.log10(np.abs(array_flux_f))
     
     filter_m_f_plus = 22.5 - 2.5*np.log10(np.abs(array_flux_f_plus))
-#    print("filter_m")
-#    print(filter_m_f)
-#    print("filter_m_plus")
-#    print(filter_m_f_plus)
     
     colour = filter_m_f_plus - filter_m_f
-#    print("colour")
-#    print(colour)
-    
+#    print(min(4.28e8*z_val))#NEGATIVE VALUES
     Abs_mag = filter_m_f - 5*np.log10(4.28e8*z_val) + 5
-#    print("Abs_mag")
-#    print(Abs_mag)
     
     return Abs_mag, colour
 #%%
 #ugriz  filters
-a , b = cal_abs_mag(t_data_SDSS,2)
+a , b = cal_abs_mag(planck_galaxies,2)
+
+plt.figure(1)
 plt.title("Colour Magnitude plot")
 plt.xlabel("Absolute magnitude(g)")
 plt.ylabel("Colour (g-r)")
-plt.scatter(a,b,s = 0.3)
-#
+plt.scatter(a,b,s = 0.45)
+
+c , d = cal_abs_mag(planck_galaxies,1)
+plt.figure(2)
+plt.title("Colour Magnitude plot")
+plt.xlabel("Absolute magnitude(u)")
+plt.ylabel("Colour (u-g)")
+plt.scatter(c,d,s = 0.45)
+
+end = timeit.default_timer()
+print(end-start)
+
 #plt.figure()
 #plt.hist(b)
-
-#extended_sub_table = subset_table(t_data,"EXTENDED",0)#subset with only extened flag equal to 0
-#one_extended_sub_table = subset_table(t_data,"EXTENDED",1)
-#
-#extended_external_sub_sub_table = subset_table(extended_sub_table,"EXT_VAL",3)#subset within reduced table with ext_val equal to 3
