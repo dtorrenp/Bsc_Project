31a15aac2c11c65410411696bb4a934fcf9db88b
Unknown <dt1716@ic.ac.uk>
2019-02-14 12:59:26 +0000

    functions

--- Open_fits.py
+++ Open_fits.py
@@ -33,39 +33,40 @@ t_data_SDSS = Table(hdu_BinTable_SDSS.data)
 
 colnames_SDSS = t_data_SDSS.colnames
 
-
-
 reduced_table = t_data_SDSS['CLASS','Z','SPECTROFLUX','GroupID','GroupSize']
 #create new table with the ojects within each compact source that has the largest flux
 # create table of those with no tag
-mask_reduced_no_tag = reduced_table['GroupID'] < 1
-reduced_no_tag = reduced_table[mask_reduced_no_tag]#CORRECT
-#print(len(reduced_no_tag))
 
-mask_reduced_groups = reduced_table['GroupID'] > 0
-reduced_groups = reduced_table[mask_reduced_groups]#CORRECT
-#print(len(reduced_groups))
-
-
-#ugriz  filters
-f = 1
-
-for i in np.arange(1,max(reduced_groups['GroupID'])+1):
-    group_mask  = reduced_groups['GroupID'] == i
-    red_group_i = reduced_groups[group_mask]
+def Select_by_flux(reduced_table,f):
+    mask_reduced_no_tag = reduced_table['GroupID'] < 1
+    reduced_no_tag = reduced_table[mask_reduced_no_tag]#CORRECT
+    #print(len(reduced_no_tag))
     
-    flux_list_filter = []
-    for v in np.arange(red_group_i['GroupSize'][0]):
-        fluxes = np.asarray(red_group_i['SPECTROFLUX'][v])
-        flux_filter = fluxes[f]
-        flux_list_filter.append(flux_filter)
-    largest_flux = max(flux_list_filter)
-    index_flux = flux_list_filter.index(largest_flux)
-    largest_flux_object = red_group_i[index_flux]
-    reduced_no_tag.add_row(largest_flux_object)
+    mask_reduced_groups = reduced_table['GroupID'] > 0
+    reduced_groups = reduced_table[mask_reduced_groups]#CORRECT
+    #print(len(reduced_groups))
+    
+    
 
-galactic_stars = reduced_no_tag[reduced_no_tag['CLASS'] == "STAR"]
-planck_galaxies = reduced_no_tag[reduced_no_tag['CLASS'] == "GALAXY"]
+    for i in np.arange(1,max(reduced_groups['GroupID'])+1):
+        group_mask  = reduced_groups['GroupID'] == i
+        red_group_i = reduced_groups[group_mask]
+        
+        flux_list_filter = []
+        for v in np.arange(red_group_i['GroupSize'][0]):
+            fluxes = np.asarray(red_group_i['SPECTROFLUX'][v])
+            flux_filter = fluxes[f]
+            flux_list_filter.append(flux_filter)
+        largest_flux = max(flux_list_filter)
+        index_flux = flux_list_filter.index(largest_flux)
+        largest_flux_object = red_group_i[index_flux]
+        reduced_no_tag.add_row(largest_flux_object)
+    
+    galactic_stars = reduced_no_tag[reduced_no_tag['CLASS'] == "STAR"]
+    planck_galaxies = reduced_no_tag[reduced_no_tag['CLASS'] == "GALAXY"]
+    
+    print(planck_galaxies)
+    return planck_galaxies,galactic_stars
 
 #%%
 
@@ -74,6 +75,7 @@ def cal_abs_mag(table,f):
     array_table_flux = np.array(table["SPECTROFLUX"])
     
     array_flux_f = np.abs(array_table_flux[:,f])
+    
     #print(array_flux_f[array_flux_f < 0])#WEIRD SHIT WITH NEGATIVE FLUXES
     
     array_flux_f_plus = np.abs(array_table_flux[:,f-1])
@@ -89,20 +91,24 @@ def cal_abs_mag(table,f):
     Abs_mag = filter_m_f - 5*np.log10(4.28e8*z_val) + 5
     
     return Abs_mag, colour
+
 #%%
 #ugriz  filters
-a , b = cal_abs_mag(planck_galaxies,2)
+    
+planck_galaxies_2,galactic_stars_2 = Select_by_flux(reduced_table, 2)
+a , b = cal_abs_mag(planck_galaxies_2,2)
 
 plt.figure(1)
 plt.title("Colour Magnitude plot")
-plt.xlabel("Absolute magnitude(g)")
+plt.xlabel("Absolute magnitude (g)")
 plt.ylabel("Colour (g-r)")
 plt.scatter(a,b,s = 0.45)
 
-c , d = cal_abs_mag(planck_galaxies,1)
+planck_galaxies_1, galactic_stars_1 = Select_by_flux(reduced_table, 1)
+c , d = cal_abs_mag(planck_galaxies_1,1)
 plt.figure(2)
 plt.title("Colour Magnitude plot")
-plt.xlabel("Absolute magnitude(u)")
+plt.xlabel("Absolute magnitude (u)")
 plt.ylabel("Colour (u-g)")
 plt.scatter(c,d,s = 0.45)
 
